f50b974a513735e0b2445911b7d00dedc9f69045
Amila Buddhika <amilagamage26@gmail.com>
2025-04-17 14:15:59 +0530

    Use official Cypress GitHub Action and simplify tests

--- e2e/cypress/e2e/basic.cy.js
+++ e2e/cypress/e2e/basic.cy.js
@@ -7,18 +7,4 @@ describe('Basic Test', () => {
     expect(true).to.equal(true);
     cy.log('Basic test passed successfully');
   });
-
-  it('should verify Cypress is working', () => {
-    // Create a simple HTML page for testing
-    cy.visit('about:blank');
-    
-    // Modify the page content
-    cy.document().then((doc) => {
-      doc.body.innerHTML = '<h1>Cypress Test Page</h1><p>This is a test page created by Cypress.</p>';
-    });
-    
-    // Verify the content
-    cy.get('h1').should('contain', 'Cypress Test Page');
-    cy.get('p').should('contain', 'This is a test page created by Cypress.');
-  });
 });
